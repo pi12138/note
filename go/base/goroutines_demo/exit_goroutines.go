b7b52baec839b0d05cb5f8d814851099201086f8
zyp <zyp@example.com>
2024-01-08 08:03:18 +0800

    feat: add test

---
+++ go/base/goroutines_demo/exit_goroutines.go
@@ -0,0 +1,78 @@
+package goroutinesdemo
+
+import (
+	"context"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func ExitGoroutine() {
+	runtime.GOMAXPROCS(1)
+	var v int = 10
+	ctx, cancel := context.WithCancel(context.TODO())
+	// c := make(chan struct{})
+
+	go GoOne(&v)
+	go GoTwo(ctx)
+	// go GoThree(c)
+
+	time.Sleep(time.Second * 4)
+	s1 := time.Now()
+	v = 0
+	s2 := time.Now()
+	cancel()
+	s3 := time.Now()
+	// c <- struct{}{}
+	s4 := time.Now()
+
+	// time.Sleep(time.Second * 5)
+	fmt.Printf("s2 - s1: %s\ns3 - s2: %s\ns4 - s3: %s\n", s2.Sub(s1), s3.Sub(s2), s4.Sub(s3))
+}
+
+func GoOne(v *int) {
+	defer func(start time.Time) {
+		fmt.Printf("GoOne cost: %s\n", time.Since(start))
+	}(time.Now())
+
+	for {
+		if *v == 0 {
+			return
+		}
+
+		// do work
+		time.Sleep(time.Second * 3)
+	}
+
+}
+
+func GoTwo(ctx context.Context) {
+	defer func(start time.Time) {
+		fmt.Printf("GoTwo cost: %s\n", time.Since(start))
+	}(time.Now())
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			time.Sleep(time.Second * 3)
+		}
+	}
+}
+
+func GoThree(c chan struct{}) {
+	defer func(start time.Time) {
+		fmt.Printf("GoThree cost: %s\n", time.Since(start))
+	}(time.Now())
+
+	for {
+		select {
+		case <-c:
+			return
+		default:
+			time.Sleep(time.Second * 3)
+		}
+	}
+
+}
---
+++ go/base/goroutines_demo/exit_goroutines_test.go
@@ -0,0 +1,18 @@
+package goroutinesdemo
+
+import "testing"
+
+func TestExitGoroutine(t *testing.T) {
+	// tests := []struct {
+	// 	name string
+	// }{
+	// 	// TODO: Add test cases.
+	// }
+	// for _, tt := range tests {
+	// 	t.Run(tt.name, func(t *testing.T) {
+	// 		ExitGoroutine()
+	// 	})
+	// }
+
+	ExitGoroutine()
+}
